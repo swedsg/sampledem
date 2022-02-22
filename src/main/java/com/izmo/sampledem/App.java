f7945c28370ed87bb4d1a70a4f2bef68061d3058
sachin singh <46350715+swedsg@users.noreply.github.com>
2022-02-22 11:33:36 +0530

    added

---
+++ src/main/java/com/izmo/sampledem/App.java
@@ -0,0 +1,13 @@
+package com.izmo.sampledem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
